13a450d91364100a094879769043162bf4bbcd4c
Thiago Felipe dos Santos <thiago.f.santos@animaeducacao.com.br>
2021-04-05 22:06:58 -0300

    Criação das classes (Series e Entidades) do Enum (Genero) e da solution.

---
+++ DIOSeries/Classes/Serie.cs
@@ -0,0 +1,52 @@
+﻿using DIOSeries.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIOSeries.Classes
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        //Metodo (Constructor)
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            //Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline
+            // o environment basicamente identifica qual o sistem operacional está 
+            //utilizando e insere a quebra de linha automaticamente.
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine; 
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Inicio: " + this.Ano;
+            return retorno;
+            
+        }
+
+        //encapsulamentos: 
+        //retornaTitulo ele retona o titulo do objeto
+        //retornaId
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        internal int retornaId()
+        {
+            return this.Id;
+        }
+    }
+}
